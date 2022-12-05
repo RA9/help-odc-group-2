4e068768f79b4aee3346d8fa2a39ac798922230f
Deabeh18 <chris.deabeh@gmail.com>
2022-12-05 13:39:37 +0000

    I created the Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-
+import React from 'react';
 import './App.css';
+
+import Services from './components/Services.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+<Services />
 
-      </header>
-    </div>
   );
 }
 
